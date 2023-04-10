31b005ee4b95d90fc6588fbeae1c1260215fcd4c
Josiney de Souza <josiney.souza@ifc.edu.br>
2023-04-10 18:44:38 -0300

    Criada função cria_certificados()
    Criada a função cria_certificados() para criar os arquivos PDF de certificados a partir dos arquivos-fontes LaTeX da função cria_fontes_tex()

--- claquete-gera-certificados.sh
+++ claquete-gera-certificados.sh
@@ -33,6 +33,21 @@ DADOS_ORGANIZACAO="dados-organizacao.csv"
 # Base de dados unica que junta todas as demais bases
 DADOS="dados.txt"
 
+# Bases de certificados em LaTeX para cada categoria que sera gerada
+BASE_OUVINTE="base-ouvinte.tex"
+BASE_PALESTRANTE="base-palestrante.tex"
+BASE_MEDIADOR="base-mediador.tex"
+BASE_ORGANIZACAO="base-organizacao.tex"
+
+# Cor do texto no certificado
+CONF_COR="black"
+
+# Dados gerais do evento
+EVENTO="III Semana Acadêmica de Informática do Instituto Federal \
+Catarinense - Campus Brusque"
+PERIODO="25\/10\/2017 a 27\/10\/2017"
+DATA=$(date +"%d de %B de %Y")
+
 
 
 ########################################
@@ -85,6 +100,121 @@ function cria_base_formatada () {
 
 
 
+#####################################
+##### FUNCAO: cria_fontes_tex() #####
+#####################################
+function cria_fontes_tex () {
+    echo -n "Criando todos os fontes do tipo .TEX... "
+
+    cat ${DADOS} | while read PARTICIPANTE
+    do
+        NOME=$(echo $PARTICIPANTE | cut -d';' -f1)
+        CPF=$(echo $PARTICIPANTE | cut -d';' -f2)
+        TIPO=$(echo $PARTICIPANTE | cut -d';' -f3)
+        HORAS=$(echo $PARTICIPANTE | cut -d';' -f4)
+        ADICIONAL=$(echo $PARTICIPANTE | cut -d';' -f5)
+
+        # Obtem o nome do participante e substitui os espacos em branco por
+        # sublinhados/underline/underscore
+        NOME_COM_UNDERLINE=$(echo $NOME | sed -e "s/ /_/g")
+
+        # Define que o nome do arquivo sera certificado-NOME-TIPO, onde
+        # NOME: nome da pessoa que recebera o certificado
+        # TIPO: tipo/categoria de participacao no evento
+        NOME_ARQUIVO="certificado-${NOME_COM_UNDERLINE}-${TIPO}"
+
+        # Verifica se uma pessoa possui mais de um certificado para o mesmo
+        # tipo/categoria de participacao
+        # Se sim, adiciona um numero/contador para anexar ao nome do arquivo
+        # LaTeX (e tambem PDF depois)
+        if [ -e ${DIR_FONTES_TEX}/${NOME_ARQUIVO}.tex ]
+        then
+            cd ${DIR_FONTES_TEX}
+            NOME_ARQUIVO=$(ls -1 ${NOME_ARQUIVO}*.tex | sort -t- -k 4 | \
+                tail -n 1 | cut -d'.' -f1)
+            NUM_CERTIFICADO=$(echo ${NOME_ARQUIVO} | cut -d'-' -f4)
+	        if [[ ${NUM_CERTIFICADO} == "" ]]
+            then
+                NUM_CERTIFICADO=1
+            else
+                ((NUM_CERTIFICADO = ${NUM_CERTIFICADO} + 1))
+            fi
+NOME_ARQUIVO="certificado-${NOME_COM_UNDERLINE}-${TIPO}-${NUM_CERTIFICADO}"
+            cd - &> /dev/null
+        fi
+
+        # Escolhe a base do certificado em LaTeX correta a depender do tipo/
+        # categoria de participacao
+        if [[ ${TIPO} == "ouvinte" ]]
+        then
+            cp ${BASE_OUVINTE} ${NOME_ARQUIVO}.tex
+        elif [[ ${TIPO} == "palestrante" ]]
+        then
+            cp ${BASE_PALESTRANTE} ${NOME_ARQUIVO}.tex
+            sed -i -e "s/TITULOP/${ADICIONAL}/g" ${NOME_ARQUIVO}.tex
+        elif [[ ${TIPO} == "mediador" ]]
+        then
+            cp ${BASE_MEDIADOR} ${NOME_ARQUIVO}.tex
+            sed -i -e "s/ASSUNTOP/${ADICIONAL}/g" ${NOME_ARQUIVO}.tex
+        elif [[ ${TIPO} == "organizacao" ]]
+        then
+            TIPO="membro da Comissão de Organização"
+            cp ${BASE_ORGANIZACAO} ${NOME_ARQUIVO}.tex
+            sed -i -e "s/ATIVIDADESP/organizador/g" ${NOME_ARQUIVO}.tex
+        else
+            echo "ERRO: tipo de participante nao existe"
+            exit 1
+        fi
+
+        # Substitui as informacoes das bases de dados nos arquivos de base do
+        # certificado em LaTeX escolhido logo acima
+        sed -i -e "s/NOMEP/${NOME}/g" -e "s/CPFP/${CPF}/g" \
+            -e "s/TIPOP/${TIPO}/g" -e "s/HORASP/${HORAS}/g" \
+            -e "s/EVENTOP/${EVENTO}/g" -e "s/PERIODOP/${PERIODO}/g" \
+            -e "s/DATAP/${DATA}/g" ${NOME_ARQUIVO}.tex
+
+        # Defini a cor do texto dos certificados com base na configuracao
+        # definida no comeco do script
+        sed -i -e "s/CONF_COR/${CONF_COR}/g" ${NOME_ARQUIVO}.tex
+
+        # Realoca o arquivo-fonte LaTeX gerado para a pasta mais adequada
+        mv ${NOME_ARQUIVO}.tex ${DIR_FONTES_TEX}
+    done
+
+    sleep 1s
+    echo "OK."
+}	
+
+
+
+#######################################
+##### FUNCAO: cria_certificados() #####
+#######################################
+function cria_certificados () {
+    echo -n "Criando todos os certificados em .PDF... "
+
+    # Para cada arquivo-fonte LaTeX gerado, cria um arquivo PDF de
+    # certificado e o guarda no diretorio/pasta de arquivos PDFs
+    # Devido a caracteristica do LaTeX, onde alguns simbolos nao sao gerados
+    # com uma unica compilacao, executa-se a compilacao duas vezes em
+    # sequencia
+    cd ${DIR_FONTES_TEX}
+    for FONTE in $(ls)
+    do
+        pdflatex -output-directory ../${DIR_CERTIFICADOS_PDF} ${FONTE} &> /dev/null
+        pdflatex -output-directory ../${DIR_CERTIFICADOS_PDF} ${FONTE} &> /dev/null
+    done
+    cd - &> /dev/null
+
+    # Remove todos os arquivos auxiliares gerados durante a criacao do PDF
+    rm -f ${DIR_CERTIFICADOS_PDF}/*.{aux,log}
+
+    sleep 1s
+    echo "OK."
+}
+
+
+
 ############################
 ##### FUNCAO PRINCIPAL #####
 ############################
@@ -121,6 +251,13 @@ else
 
     # Cria uma base de dados unica juntando todas as demais bases de dados
     cria_base_formatada
+
+    # Cria os arquivos-fonte em LaTeX como passo intermediario antes de gerar
+    # os arquivos PDF
+    cria_fontes_tex
+
+    # Cria os arquivos PDF dos certificados
+    cria_certificados
 fi
 
 exit 0
